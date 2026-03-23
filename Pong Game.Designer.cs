57e20fc0f8def21c436d6a25c814a7e42a4c1a9a
MariamYousry1234 <mariamyousry030@gmail.com>
2026-03-23 15:47:50 +0200

    Add project files.

---
+++ Pong Game.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Pong_Game
+{
+    partial class PongGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblPlayer2Score = new System.Windows.Forms.Label();
+            this.lblPlayer1Score = new System.Windows.Forms.Label();
+            this.pnWallPlayer1 = new System.Windows.Forms.Panel();
+            this.pnWallPlayer2 = new System.Windows.Forms.Panel();
+            this.pnBall = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblPlayer2Score
+            // 
+            this.lblPlayer2Score.AutoSize = true;
+            this.lblPlayer2Score.BackColor = System.Drawing.Color.Transparent;
+            this.lblPlayer2Score.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayer2Score.ForeColor = System.Drawing.Color.White;
+            this.lblPlayer2Score.Location = new System.Drawing.Point(445, 22);
+            this.lblPlayer2Score.Name = "lblPlayer2Score";
+            this.lblPlayer2Score.Size = new System.Drawing.Size(68, 73);
+            this.lblPlayer2Score.TabIndex = 0;
+            this.lblPlayer2Score.Text = "0";
+            // 
+            // lblPlayer1Score
+            // 
+            this.lblPlayer1Score.AutoSize = true;
+            this.lblPlayer1Score.BackColor = System.Drawing.Color.Transparent;
+            this.lblPlayer1Score.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayer1Score.ForeColor = System.Drawing.Color.White;
+            this.lblPlayer1Score.Location = new System.Drawing.Point(595, 22);
+            this.lblPlayer1Score.Name = "lblPlayer1Score";
+            this.lblPlayer1Score.Size = new System.Drawing.Size(68, 73);
+            this.lblPlayer1Score.TabIndex = 1;
+            this.lblPlayer1Score.Text = "0";
+            // 
+            // pnWallPlayer1
+            // 
+            this.pnWallPlayer1.BackColor = System.Drawing.Color.White;
+            this.pnWallPlayer1.Location = new System.Drawing.Point(1050, 220);
+            this.pnWallPlayer1.Name = "pnWallPlayer1";
+            this.pnWallPlayer1.Size = new System.Drawing.Size(30, 116);
+            this.pnWallPlayer1.TabIndex = 2;
+            // 
+            // pnWallPlayer2
+            // 
+            this.pnWallPlayer2.BackColor = System.Drawing.Color.White;
+            this.pnWallPlayer2.Location = new System.Drawing.Point(0, 220);
+            this.pnWallPlayer2.Name = "pnWallPlayer2";
+            this.pnWallPlayer2.Size = new System.Drawing.Size(30, 116);
+            this.pnWallPlayer2.TabIndex = 3;
+            // 
+            // pnBall
+            // 
+            this.pnBall.BackColor = System.Drawing.Color.White;
+            this.pnBall.Location = new System.Drawing.Point(539, 276);
+            this.pnBall.Name = "pnBall";
+            this.pnBall.Size = new System.Drawing.Size(21, 24);
+            this.pnBall.TabIndex = 4;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // PongGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1081, 578);
+            this.Controls.Add(this.pnBall);
+            this.Controls.Add(this.pnWallPlayer2);
+            this.Controls.Add(this.pnWallPlayer1);
+            this.Controls.Add(this.lblPlayer1Score);
+            this.Controls.Add(this.lblPlayer2Score);
+            this.MaximizeBox = false;
+            this.Name = "PongGame";
+            this.Text = " ";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.PongGame_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.PongGame_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPlayer2Score;
+        private System.Windows.Forms.Label lblPlayer1Score;
+        private System.Windows.Forms.Panel pnWallPlayer1;
+        private System.Windows.Forms.Panel pnWallPlayer2;
+        private System.Windows.Forms.Panel pnBall;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Pong Game.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pong_Game
+{
+    public partial class PongGame : Form
+    {
+        public PongGame()
+        {
+            InitializeComponent();
+        }
+
+        enum enWinner
+        {
+            Player1,
+            Player2,
+            None
+        }
+        enWinner Winner = enWinner.None;
+
+        int BallStep =5;
+        int P1Score = 0;
+        int P2Score = 0;
+
+        int YDirection = -1;
+        int XDirection = 1;
+        Random rn = new Random();
+        private void PongGame_Paint(object sender, PaintEventArgs e)
+        {
+            Color White = Color.FromArgb(255, 255, 255);
+
+            Pen pen = new Pen(White);
+            pen.Width = 1;
+
+
+            pen.StartCap = System.Drawing.Drawing2D.LineCap.Round;
+            pen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+            e.Graphics.DrawLine(pen, this.Width/2, 10, this.Width/2, 550);
+        }
+
+
+
+
+        void GoUp(Keys key)
+        {
+            if(key == Keys.Up)
+            {
+                pnWallPlayer1.Location=new Point(pnWallPlayer1.Location.X, Math.Max(pnWallPlayer1.Location.Y-10,0));
+                return;
+            }
+
+            if(key == Keys.W)  
+                pnWallPlayer2.Location = new Point(pnWallPlayer2.Location.X, Math.Max(pnWallPlayer2.Location.Y - 10, 0));
+         
+        }
+
+
+        void GoDown(Keys key)
+        {
+            if (key == Keys.Down)
+            {        
+                pnWallPlayer1.Location = new Point(pnWallPlayer1.Location.X, Math.Min(pnWallPlayer1.Location.Y + 10, this.ClientSize.Height-pnWallPlayer1.Height));
+                return;
+            } 
+
+            if (key == Keys.S)
+            
+                pnWallPlayer2.Location = new Point(pnWallPlayer2.Location.X, Math.Min(pnWallPlayer2.Location.Y + 10, this.ClientSize.Height - pnWallPlayer2.Height));
+          
+        }
+        
+        void Move(Keys key)
+        {
+            if(key == Keys.W || key ==Keys.Up)
+            {
+                GoUp(key);
+                return;
+            }
+
+            if(key == Keys.S || key == Keys.Down)
+            {
+                GoDown(key);
+            }
+        }
+
+        private void PongGame_KeyDown(object sender, KeyEventArgs e)
+        {
+            Move(e.KeyData);
+        }
+
+        void ResetRound()
+        {
+            YDirection = rn.Next(0, 2) == 0 ? -1 : 1;
+            XDirection = rn.Next(0, 2) == 0 ? -1 : 1;
+            pnBall.Location = new Point(this.Width / 2, this.Height / 2);
+        }
+        void EndRound(enWinner Winner)
+        {
+            if(Winner == enWinner.Player1)
+            {
+                P1Score++;
+                lblPlayer1Score.Text = P1Score.ToString();
+            }
+
+            if(Winner ==enWinner.Player2)
+            {
+                P2Score++;
+                lblPlayer2Score.Text = P2Score.ToString();
+            }
+            ResetRound();
+        }
+
+        bool IsGameOver()
+        {
+            return P1Score == 10 || P2Score == 10;
+        }
+        void ResetGame()
+        {
+             Winner = enWinner.None;
+             P1Score = 0;
+             P2Score = 0;
+             lblPlayer1Score.Text = "0";
+             lblPlayer2Score.Text = "0";
+             ResetRound();
+        }
+        void EndGame()
+        {
+            string Message = "";
+            Message = (P1Score == 10) ? "Player1 Win\nDo you want to paly again?" :
+                "Player2 Win\nDo you want to play again?";
+
+            if (MessageBox.Show(Message, "Game Over!", MessageBoxButtons.OKCancel) == DialogResult.OK)
+            {
+                ResetGame();
+                timer1.Start();
+            }
+            else
+                this.Close();
+           
+        }
+
+        void MoveTheBall()
+        {
+            pnBall.Location = new Point(pnBall.Location.X + (BallStep * XDirection), pnBall.Location.Y + (BallStep *YDirection));
+
+            if (pnBall.Location.Y <= 0 ||
+                pnBall.Location.Y >= this.ClientSize.Height - pnBall.Height)     
+                YDirection *= -1;
+        
+
+            if(pnBall.Bounds.IntersectsWith(pnWallPlayer1.Bounds) && XDirection > 0 ||
+               pnBall.Bounds.IntersectsWith(pnWallPlayer2.Bounds) && XDirection < 0)
+                XDirection *= -1;
+           
+            if(pnBall.Location.X > this.ClientSize.Width)
+            {
+                EndRound(enWinner.Player2);
+            }
+
+            if(pnBall.Location.X < 0)
+            {
+                EndRound(enWinner.Player1);
+            }
+
+            if(IsGameOver())
+            {
+                timer1.Stop();
+                EndGame();
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            MoveTheBall();
+        }
+
+       
+    }
+}
